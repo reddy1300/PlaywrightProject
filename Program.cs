a996002a2f1aa21b3880b7492c74c8e2040faf88
Nagendra Reddy <nagendrareddy29029@gmail.com>
2025-08-06 19:52:23 +0530

    Project Setup

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://google.com");
+        await Task.Delay(5000); // Wait for 5 seconds to see the page
+    }
+}
